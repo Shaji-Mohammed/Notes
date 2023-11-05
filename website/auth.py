7093f1d9eed5fe3ea220e959a5718f74bfe35b6c
Shaji-Mohammed <mshajisufiyan@gmail.com>
2023-11-04 21:26:08 -0300

    created alerts using flash()

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,10 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, flash, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
     return render_template("login.html")
 
 
@@ -17,4 +15,21 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 5:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(name) < 2:
+            flash('Name must be greater than 1 character.', category='error')
+        elif len(password1) < 6:
+            flash('password must be greater than 5 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        else:
+            flash('Account created!', category='success')
+            # add user
     return render_template("sign_up.html")
